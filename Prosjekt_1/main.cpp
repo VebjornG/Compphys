fe53be6eab77bffdda8c1ea9b0074ba8c61410f2
VebjornG <vebjorngilberg@gmail.com>
2016-09-19 00:35:21 +0200

    Add files via upload

--- Prosjekt_1/main.cpp
+++ Prosjekt_1/main.cpp
@@ -5,141 +5,10 @@
 #include <string>
 #include <cstdlib> //atoi
 #include "time.h" //
+#include <armadillo>
 
 using namespace std;
-
-
-// General solution with 9(n-1) flops
-double gen_solv(int n, double* v) {
-
-    // Declaration
-    double *a = new double[n+2];
-    double *f = new double[n+2];
-    double *x = new double[n+2];
-    double *b = new double[n+2];
-    double *c = new double[n+2];
-    double *f_tilde = new double[n+2];
-    double *b_tilde = new double[n+2];
-    //double *runtime_gen = new double[n];
-    double h = 1.0/(n+1);
-
-    //Setting up the matrices
-    for (int i = 0; i < n+2; i++) {
-        x[i] = i*h;
-        v[i] = 0.0;
-        a[i] = -1.0;
-        c[i] = -1.0;
-        b[i] = 2.0;
-        f[i] = h*h*100*exp(-10*x[i]);
-    }
-    //Initial conditions
-    f_tilde[1] = f[1];
-    b_tilde[1] = b[1];
-
-    clock_t start, finish;
-
-    // start clock for gen_solv
-    start = clock();
-
-    // Forward substitution
-    for (int i = 2; i < n+1; i++) {
-        b_tilde[i] = b[i] - (a[i-1]*c[i-1]/b_tilde[i-1]);
-        f_tilde[i] = f[i] - (a[i-1]*f_tilde[i-1]/b_tilde[i-1]);
-    }
-
-    // 10th(and 100th and 1000th) element between the endpoints of the 12 points for n = 10 and so on.
-    v[n] = f_tilde[n]/b_tilde[n];
-
-    // Backward substitution
-    for (int i = n-1; i >= 1; i--) {
-        v[i] = (f_tilde[i] - v[i+1]*c[i])/b_tilde[i];
-    }
-
-    // end clock
-    finish = clock();
-
-    // Time used calculating in the general case
-    /*double runtime_gen = 0;
-    for (int i = 0; i <= n; i++) {
-        double runtime_gen[i] = ((finish - start)/double(CLOCKS_PER_SEC));
-    }*/
-
-    //double average_gen = runtime_gen/n;
-    double runtime_gen = ((finish - start)/double(CLOCKS_PER_SEC));
-    //cout << average_gen << endl;
-    cout << setiosflags(ios::showpoint | ios::uppercase);
-    cout << setprecision(25) << setw(15) << "The time used in the general case = " << runtime_gen << endl;
-    return runtime_gen;
-};
-// End gen_sol
-
-// Start special solver
-double Special_solv(int n, double* v_) {
-
-    // Declaration
-    double *f = new double[n+2];
-    double *x = new double[n+2];
-    double *b = new double[n+2];
-    double *f_tilde = new double[n+2];
-    double *b_tilde = new double[n+2];
-    double *z = new double[n+2];
-    double h = 1.0/(n+1);
-
-    //initialize elements
-    for (int i = 0; i < n+1; i++) {
-        x[i] = i*h;
-        b[i] = 2.0;
-        f[i] = h*h*100*exp(-10*x[i]);
-
-    }
-
-    // Calculating z and b_tilde
-    for (int i = 1; i < n+2; i++) {
-        b_tilde[i] = (i+1.0)/(i);
-        z[i] = (i-1.0)/(i); // This is to reduce the amount of flops in f_tilde and v_
-    }
-
-    // Initial conditions
-    f_tilde[1] = f[1];
-    b_tilde[1] = b[1];
-
-
-    clock_t start, finish;
-
-    // start clock for Special_solv
-    start = clock();
-
-    // Looping over the algorithms from the methods-section - Forward substitution
-    for (int i = 2; i < n+1; i++) {
-        f_tilde[i] = f[i] + (z[i]*f_tilde[i-1]);
-    }
-
-    // 10th(and 100th and 1000th) element between the endpoints of the 12 points for n = 10 and so on.
-
-    v_[n] = f_tilde[n]/b_tilde[n];
-
-    // Backward substitution
-    for (int i = n-1; i >= 1; i--) {
-        v_[i] = (f_tilde[i] + v_[i+1])/b_tilde[i];
-    }
-
-    // end clock
-    finish = clock();
-
-    // Time used calculating in the special case
-    double runtime_special = ((finish - start)/double(CLOCKS_PER_SEC));
-    cout << setiosflags(ios::showpoint | ios::uppercase);
-    cout << setprecision(25) << setw(15) << "The time used in the special case = " << runtime_special << endl;
-    return runtime_special;
-};
-
-/*double epsilon(double *u, double *v) {
-    return double log10(fabs((v - u)/u)
-
-    for (i = 0) {
-
-    }
-}*/
+using namespace arma;
 
 int main(int argc, char* argv[]) {
 
@@ -152,24 +21,49 @@ int main(int argc, char* argv[]) {
 
     // Declaring variables
     int n = atoi(argv[2]); // second command line arg
-    double *v = new double[n+2];
-    double *v_ = new double[n+2];
     int spacing = 15;
 
-    // Calling the solvers to write the times and v, v_ to file
-    double t_general = gen_solv(n, v);
-    double t_special = Special_solv(n, v_);
+    vec b = zeros<vec>(n);
+    vec f = zeros<vec>(n);
+    vec x(n);
+    mat A = zeros<mat>(n, n);
+    double h = 1.0/(n+1);
+
+    A(0, 0) = 2.0; A(0, 1) = -1.0; x(0) = h; b(0) = f(x(0));
+    x(n-1) = x(0)+ (n-1)*h; b(n-1) = f(x(n-1));
+
+
+    for (int i = 1; i < n-1; i++) {
+       x(i) = x(i-1) + h;
+        b(i) = f(i);
+        A(i, i-1) = -1.0;
+        A(i, i) = 2.0;
+        A(i, i+1) = -1.0;
+    }
+
+    A(n-1,n-1) = 2.0; A(n-2, n-1) = -1.0; A(n-1, n-2) = -1.0;
+
+    //Measuring runtime of LU decomposition
+    clock_t start, finish;
+
+    start = clock();
+
+
+    // Solution for Av = f
+    vec z = solve(A,f);
+
+    finish = clock();
+
+    // Time used calculating in the special case
+    double runtime_LU = ((finish - start)/double(CLOCKS_PER_SEC));
+    cout << setiosflags(ios::showpoint | ios::uppercase);
+    cout << setprecision(25) << setw(15) << "Runtime of the LU decomposition = " << runtime_LU << endl;
 
     // Writing to file
     ofstream ofile;
     ofile.open((string(argv[1]) + "-" + string(argv[2]) + ".txt").c_str());
-    ofile << setw(spacing + 5) << setprecision(8) << "Time used gen_solv: " << t_general << endl;
-    ofile << setw(spacing + 5) << setprecision(8) << "Time used Special_solv: "<< t_special << endl;
-    ofile << "v:" << setw(spacing + 5) << "v_:" << endl;
-    for (int i = 0; i < n+2; i++) {
-        ofile << setw(spacing) << setprecision(8) << v[i];
-        ofile << setw(spacing + 5) << setprecision(8) << v_[i] << endl;
-    }
+    ofile << setw(spacing + 5) << setprecision(8) << "Time used running the LU decomposition: "<< runtime_LU << endl;
+    ofile << setprecision(8) << A << endl;
 
     ofile.close();
 
